9e5d92a8c8f7d3f7cd0c8e2e326ffa0f722ff1be
backendpython <holmirzaevsohruh196@gmail.com>
2022-11-16 16:13:06 +0500

    uptade Rect

--- main.js
+++ main.js
@@ -4,17 +4,23 @@ window.addEventListener('load', function () {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 
-    const image1 = document.getElementById('image1')
+    const image1 = document.getElementById('image1');
 
     class Particle {
         constructor(effect){
             this.effect = effect;
             this.x = Math.random() * this.effect.width;
             this.y = Math.random() * this.effect.height;
-            this.size = 100;
+            this.size = 15;
+            this.vx = Math.random() * 2 - 1;
+            this.vy = Math.random() * 2 - 1;
         }
         draw(context){
             context.fillRect(this.x, this.y, this.size, this.size);
+        }
+        uptade(){
+            this.x += this.vx;
+            this.y += this.vy;
         }
     }
 
@@ -31,7 +37,7 @@ window.addEventListener('load', function () {
 
         }
         init(){
-            for (let i = 0; i < 10; i++) {
+            for (let i = 0; i < 100; i++) {
                 this.particlesArray.push(new Particle(this))
             }
         }
@@ -39,14 +45,20 @@ window.addEventListener('load', function () {
             this.particlesArray.forEach(particle => particle.draw(context));
             context.drawImage(this.image, this.x, this.y);
         }
+        uptade(){
+            this.particlesArray.forEach(particle => particle.uptade());
+        }
     }
 
     const effect = new Effect(canvas.width, canvas.height);
     effect.init();
-    effect.draw(ctx);
 
     function animate() {
-        
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        effect.draw(ctx);
+        effect.uptade();
+        window.requestAnimationFrame(animate);
     }
+    animate();
 
 })
