3953b8b28f3f655063cb1b604a810b5e627ee6a8
backendpython <holmirzaevsohruh196@gmail.com>
2022-11-16 17:04:00 +0500

    color and ease

--- main.js
+++ main.js
@@ -7,20 +7,30 @@ window.addEventListener('load', function () {
     const image1 = document.getElementById('image1');
 
     class Particle {
-        constructor(effect){
+        constructor(effect, x, y, color){
             this.effect = effect;
             this.x = Math.random() * this.effect.width;
-            this.y = Math.random() * this.effect.height;
-            this.size = 15;
-            this.vx = Math.random() * 2 - 1;
-            this.vy = Math.random() * 2 - 1;
+            this.y = 0;
+            this.originX = Math.floor(x);
+            this.originY = Math.floor(y);
+            this.color = color;
+            this.size = this.effect.gap;
+            this.vx = 0;
+            this.vy = 0;
+            this.ease = 0.1;
         }
         draw(context){
+            context.fillStyle = this.color;
             context.fillRect(this.x, this.y, this.size, this.size);
         }
         uptade(){
-            this.x += this.vx;
-            this.y += this.vy;
+            this.x += (this.originX - this.x) * this.ease;
+            this.y += (this.originY - this.y) * this.ease;
+        }
+        warp(){
+            this.x = Math.random() * this.effect.width;
+            this.y = Math.random() * this.effect.height;
+            this.ease = 0.5
         }
     }
 
@@ -34,24 +44,41 @@ window.addEventListener('load', function () {
             this.centerY = this.height * 0.5;
             this.x = this.centerX - this.image.width * 0.5;
             this.y = this.centerY - this.image.height * 0.5;
-
+            this.gap = 1;
         }
-        init(){
-            for (let i = 0; i < 100; i++) {
-                this.particlesArray.push(new Particle(this))
+        init(context){
+            context.drawImage(this.image, this.x, this.y);
+            const pixels = context.getImageData(0, 0, this.width, this.height).data;
+            for (let y = 0; y < this.height; y += this.gap){
+                for (let x = 0; x < this.width; x += this.gap){
+                    const index = (y * this.width + x) * 4;
+                    const red = pixels[index];
+                    const green = pixels[index + 1];
+                    const blue = pixels[index + 2];
+                    const alpha = pixels[index + 3];
+                    const color = 'rgb(' + red +  ',' + green + ',' + blue + ')';
+
+                    if (alpha > 0) {
+                        this.particlesArray.push(new Particle(this, x, y, color))
+                    }
+                }
             }
+            console.log(pixels);
         }
         draw(context){
             this.particlesArray.forEach(particle => particle.draw(context));
-            context.drawImage(this.image, this.x, this.y);
         }
         uptade(){
             this.particlesArray.forEach(particle => particle.uptade());
         }
+        warp(){
+            this.particlesArray.forEach(particle => particle.warp());
+        }
     }
 
     const effect = new Effect(canvas.width, canvas.height);
-    effect.init();
+    effect.init(ctx);
+    console.log(effect);
 
     function animate() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -60,5 +87,7 @@ window.addEventListener('load', function () {
         window.requestAnimationFrame(animate);
     }
     animate();
+
+    const warpButton = this.document.getElementById('warpButton')
 
 })
